ee99f4408205a21de1c93f3616410dce3bd53def
jones <eng.njokosi@gmail.com>
2021-01-05 01:30:23 +0530

    Change example button styles

--- tutorial/src/components/button.js
+++ tutorial/src/components/button.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const ExampleButton = styled.button`
-    background: green;
-    color: orange;
-    font-size: 2rem;
-`
+  font-style: normal;
+  font-weight: 600;
+  display: inline-block;
+  padding: 12px 32px;
+  font-size: 16px;
+  line-height: normal;
+  text-align: center;
+  border: 2px solid transparent;
+  border-radius: 3px;
+  outline: 0;
+  box-shadow: 0 2px 4px 0 #c8d7ee;
+  transition: all 0.2s ease-in-out;
+  color: #3368fa;
+  background-color: #fff;
+  border-color: #3368fa;
+`;
